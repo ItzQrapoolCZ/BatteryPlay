e2b705a0d381f2d79f1fefc9b1229707f3368975
ItzQrapoolCZ <danfrydl8@seznam.cz>
2025-03-21 21:03:38 +0100

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -10,9 +10,7 @@ module.exports = {
     run: async (client, interaction, lang) => {
         try {
             const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const youtubeLink = "https://www.youtube.com/";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
@@ -23,11 +21,8 @@ module.exports = {
                 })
                 .setDescription(lang.support.embed.description
                     .replace("{supportServerLink}", supportServerLink)
-                    .replace("{githubLink}", githubLink)
-                    .replace("{replitLink}", replitLink)
                     .replace("{youtubeLink}", youtubeLink)
                 )
-                .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
                 .setTimestamp();
 
             await interaction.reply({ embeds: [embed] });
