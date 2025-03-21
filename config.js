336bcbad899c7a47da862933de7d97dddd77942e
ItzQrapoolCZ <danfrydl8@seznam.cz>
2025-03-21 20:12:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://itz_qrapool:yVGr5XJmLAtaVtJ@cluster0.uvllcyc.mongodb.net/?appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
